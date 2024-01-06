ba33e90aa3229ec5e259d72e68a998d292e16648
medLmtToInfinity <med.lmt012@gmail.com>
2024-01-06 23:39:34 +0100

    Adding Sign in and Sign up Pages

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,14 @@ buildscript {
         google()
     }
     dependencies {
+        classpath("com.google.gms:google-services:4.4.0")
         val navVersion = "2.7.5"
         //navigation componenet
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
     }
 }
 plugins {
-    id("com.android.application") version "8.2.0" apply false
+    id("com.android.application") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     //Ksp: kotlin symbole process uderstaded just kutlin much faster then kapt(java)
     id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
